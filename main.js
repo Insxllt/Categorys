db3d716edda5f691a41bda56eaffdb620b17db52
Insxllt <91226831+Insxllt@users.noreply.github.com>
2023-11-01 22:53:21 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -27,63 +27,26 @@ function openNav() {
   document.getElementById("mySidenav").style.width = "505px";
 }
 
-/* Set the width of the side navigation to 0 */
+
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    var total = 0;
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     var openBox;
-//     var dataDisplay = document.getElementById("dataDisplay");
-    
-//     for (let i = 0; i < localStorage.length; i++) {
-//         var key = localStorage.key(i);
-//         if (key.includes("CategoryName")) { 
-//             var value = localStorage.getItem(key);
-            
-//             var amountKey = localStorage.key(i);
-//             var amountVal = localStorage.getItem(amountKey);
+    for (let i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
 
-//             var createPara = document.createElement("p");
-//             var box = document.createElement("div");
-//             var boxText = document.createElement("p");
-            
-            
-//             const node = document.createTextNode(value);
-            
-//             createPara.appendChild(node);
-//             createPara.appendChild(box);
-//             box.classList.add("box");
-//             dataDisplay.appendChild(createPara);
-            
-//             createPara.addEventListener("click", function() {
-//                 var associatedBox = this.querySelector(".box");
-                
-//                 if (associatedBox === openBox) {
-//                     associatedBox.style.visibility = "hidden";
-//                     associatedBox.style.height = "0";
-//                     openBox = null;
-//                 } else {
-//                     if (openBox) {
-//                         openBox.style.visibility = "hidden";
-//                         openBox.style.height = "0";
-//                     }
-//                     associatedBox.style.visibility = "visible";
-//                     associatedBox.style.left = 13 + "%";
-//                     associatedBox.style.width =  750 + "px";
-//                     associatedBox.style.height = 475 + "px";
-//                     associatedBox.style.transition = "height 1.3s";
-
-//                     boxText.textContent = "Amount: " + amountVal;
-//                     associatedBox.appendChild(boxText);
-
-//                     openBox = associatedBox;
-//                 }
-//             })  
-//         } 
-//     }
-// })
+        if(key.includes("Amount")) {
+            var amount = parseFloat(localStorage.getItem(key));
+            if (!isNaN(amount)) {
+                total = total + amount;
+            }
+        }
+    }
+    document.getElementById("box-money").innerHTML = "$" + total;
+})
 
 document.addEventListener("DOMContentLoaded", function() {
     var openBox;
@@ -144,73 +107,9 @@ document.addEventListener("DOMContentLoaded", function() {
                         associatedBox.appendChild(boxText);
                     }                    
                 }
-
-                openBox = associatedBox;
-            }
-        })  
-    }
+                    openBox = associatedBox;
+                }
+            })  
+        }
     }
 })
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     var openBox;
-//     var dataDisplay = document.getElementById("dataDisplay");
-//     var amounts = [];
-    
-//     for (let i = 0; i < localStorage.length; i++) {
-//         var key = localStorage.key(i);
-
-//         if (key.includes("CategoryName")) {
-//             var value = localStorage.getItem(key);    
-//             var keySwap = key.replace("CategoryName", "Amount")
-//             var amountVal = localStorage.getItem(keySwap);
-//             amounts.push(value);
-//             amounts.push(amountVal);
-//             console.log(amounts)
-            
-//             var createPara = document.createElement("p");
-//             var box = document.createElement("div");
-//             var boxText = document.createElement("p");
-            
-            
-//             const node = document.createTextNode(value);
-            
-//             createPara.appendChild(node);
-//             createPara.appendChild(box);
-//             box.classList.add("box");
-//             dataDisplay.appendChild(createPara);
-            
-//             createPara.addEventListener("click", function() {
-//                 var associatedBox = this.querySelector(".box");
-                
-//                 if (associatedBox === openBox) {
-//                     associatedBox.style.visibility = "hidden";
-//                 associatedBox.style.height = "0";
-//                 openBox = null;
-//             } else {
-//                 if (openBox) {
-//                     openBox.style.visibility = "hidden";
-//                     openBox.style.height = "0";
-//                 }
-//                 associatedBox.style.visibility = "visible";
-//                 associatedBox.style.left = 13 + "%";
-//                 associatedBox.style.width =  750 + "px";
-//                 associatedBox.style.height = 475 + "px";
-//                 associatedBox.style.transition = "height 1.3s";
-                
-//                 for (let j = 0; j < amounts.length; j++) {
-//                     if (this.textContent === amounts[j]) {
-//                         boxText.textContent = "Amount: " + amounts[j + 1];
-//                     }                    
-//                 }
-
-//                 associatedBox.appendChild(boxText);
-//                 openBox = associatedBox;
-//             }
-//         })  
-//     }
-//     }
-// })
-
-
-
